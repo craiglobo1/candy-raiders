a1c642e63961ac23fccf90532d038eb6964d9e4b
craiglobo1 <craiglobo1@gmail.com>
2022-07-30 10:59:00 -0600

    idk man

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ class Game:
         self.player = Player(width*0.05, height*0.9)
         self.run()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     def main_menu(self,win,):
         self.playing = True
         pos = pygame.mouse.get_pos()
@@ -49,33 +47,10 @@ class Game:
                         if quit_button.selected(pos):
                             self.playing = False
 
-=======
-    def main_menu(self,win,):
-        self.playing = True
-        while self.playing == True:
-            bg_colour= (3, 244, 252)
-            self.win.fill(bg_colour)
-            ellipsis_colour=(245, 78, 200)
-            pygame.draw.rect
-            rect = pygame.Rect(50,100,600,400)
-            pygame.draw.ellipse(self.win,ellipsis_colour, rect, width == 0)
-            font = pygame.font.SysFont('Corbel',60,bold=pygame.font.Font.bold)
-            font_colour = (237, 192, 225)
-            game_title = pygame.font.Font.render(font,'Candy Raiders', True, font_colour)
-            self.win.blit(game_title,(100,400))
-            pygame.font.get_fonts()
->>>>>>> 3befc4c61cc2be535397db33eb4148d014aeb2d2
             
             pygame.display.flip()
 
 
-<<<<<<< HEAD
-=======
-    def main_menu(self):
-        pass
->>>>>>> 5befa1f90ae391928160e901873cce479ffc656c
-=======
->>>>>>> 3befc4c61cc2be535397db33eb4148d014aeb2d2
 
     def run(self):
 
@@ -83,11 +58,7 @@ class Game:
 
         self.playing = True
         while self.playing:
-<<<<<<< HEAD
             self.dt = self.clock.tick(FPS) * .001 * FPS
-=======
-            self.dt = self.clock.tick(120)
->>>>>>> 3befc4c61cc2be535397db33eb4148d014aeb2d2
             self.events()
             self.update()
             self.draw()
@@ -99,15 +70,8 @@ class Game:
             if event.type == pygame.QUIT:
                 if self.playing:
                     self.playing = False
-<<<<<<< HEAD
 
 
-=======
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.player.shoot()
->>>>>>> 5befa1f90ae391928160e901873cce479ffc656c
         
         self.player.move(keys[pygame.K_RIGHT]|keys[pygame.K_d], keys[pygame.K_LEFT]|keys[pygame.K_a])
         
