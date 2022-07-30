0487b03647b8ff4c960531f551136f8f3ccdb67a
aridge03 <94921692+aridge03@users.noreply.github.com>
2022-07-29 18:33:47 -0600

    changed time

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Game:
 
         self.playing = True
         while self.playing:
-            self.dt = self.clock.tick(60)
+            self.dt = self.clock.tick(120)
             self.events()
             self.update()
             self.draw()
